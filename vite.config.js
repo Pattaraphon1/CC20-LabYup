9ad0f3eff30da0d6380d9e0a82a5c2750dfb7cc4
pattaraphon1 <klgsa51@gmail.com>
2025-05-21 17:22:47 +0700

    edit base name

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base: "CC20-LabYup",
+  base: "/CC20-LabYup/",
 })
